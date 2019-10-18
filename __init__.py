3d1504d28a3cb7d7e3b9e8ffeb55e0a5c3f1b518
marcerotten <marce.rotten@gmail.com>
2019-10-18 11:31:11 -0300

    first commit

---
+++ __init__.py
@@ -0,0 +1,73 @@
+# coding: utf-8
+"""
+Base para desarrollo de modulos externos.
+Para obtener el modulo/Funcion que se esta llamando:
+     GetParams("module")
+
+Para obtener las variables enviadas desde formulario/comando Rocketbot:
+    var = GetParams(variable)
+    Las "variable" se define en forms del archivo package.json
+
+Para modificar la variable de Rocketbot:
+    SetVar(Variable_Rocketbot, "dato")
+
+Para obtener una variable de Rocketbot:
+    var = GetVar(Variable_Rocketbot)
+
+Para obtener la Opcion seleccionada:
+    opcion = GetParams("option")
+
+
+Para instalar librerias se debe ingresar por terminal a la carpeta "libs"
+    
+    pip install <package> -t .
+
+"""
+from time import sleep
+import json
+import requests
+
+
+"""
+    Obtengo el modulo que fueron invocados
+"""
+module = GetParams("module")
+
+"""
+    Resuelvo catpcha tipo reCaptchav2
+"""
+
+def ocr_space_file(filename, overlay=False, api_key='helloworld', language='eng'):
+    """ OCR.space API request with local file.
+        Python3.5 - not tested on 2.7
+    :param filename: Your file path & name.
+    :param overlay: Is OCR.space overlay required in your response.
+                    Defaults to False.
+    :param api_key: OCR.space API key.
+                    Defaults to 'helloworld'.
+    :param language: Language code to be used in OCR.
+                    List of available language codes can be found on https://ocr.space/OCRAPI
+                    Defaults to 'en'.
+    :return: Result in JSON format.
+    """
+
+    payload = {'isOverlayRequired': overlay,
+               'apikey': api_key,
+               'language': language,
+               }
+    with open(filename, 'rb') as f:
+        r = requests.post('https://api.ocr.space/parse/image',
+                          files={filename: f},
+                          data=payload,
+                          )
+    return r.json()
+
+if module == "GetOCR":
+    File = GetParams("File")
+    Key = GetParams("key")
+    var_ = GetParams("result")    
+    data = ocr_space_file(File,overlay=True, api_key=Key)
+    try:
+        SetVar( var_,  data)
+    except Exception as e:
+        raise Exception(e)
